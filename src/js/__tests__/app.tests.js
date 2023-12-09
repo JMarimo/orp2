dd731013f5f40ae33c94a4017a6c84aa4a00aa45
Jane Marimo <jenny.bulatova@gmail.com>
2023-12-09 13:17:58 +0300

    Задание выполнено

---
+++ src/js/__tests__/app.tests.js
@@ -0,0 +1,58 @@
+
+import getSpeсial from '../app';
+
+test('test function getSpeсial', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
+  const result = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+  const res = getSpeсial(character);
+  expect(res).toEqual(result);
+});
+
+test('test function getSpeсial without special', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+  };
+  const result = [];
+  const res = getSpeсial(character);
+  expect(res).toEqual(result);
+});
+
+
